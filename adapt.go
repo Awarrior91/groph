f7d9e1eba1f08f6d569795e6580fef12675dc1e9
Marcus <marcus@fractalqb.de>
2018-03-11 18:01:52 +0100

    TSP

--- adapt.go
+++ adapt.go
@@ -1,9 +1,13 @@
 package groph
 
 import (
+	"fmt"
 	"reflect"
 )
 
+// SliceNMeasure implements a metric RGraph based on a slice of vertices of
+// some type V and a function f(u V, v V) → W that compute the weight of type W
+// for any edge (u, v).
 type SliceNMeasure struct {
 	slc reflect.Value
 	m   reflect.Value
@@ -22,6 +26,28 @@ func NewSliceNMeasure(
 	return res
 }
 
+// Check does type checking on g. It always returns g. Only if everything is OK
+// the returned error is nil.
+func (g *SliceNMeasure) Check() (*SliceNMeasure, error) {
+	if g.slc.Kind() != reflect.Slice {
+		return g, fmt.Errorf("vertex set has to be a slice, got %s",
+			g.slc.Type().String())
+	}
+	if g.m.Kind() != reflect.Func {
+		return g, fmt.Errorf("edge weight measure must be a function, got: %s",
+			g.m.Type().String())
+	} // TODO more precise checking
+	return g, nil
+}
+
+// Verify call Check on g and panics if Check returns an error.
+func (g *SliceNMeasure) Verify() *SliceNMeasure {
+	if _, err := g.Check(); err != nil {
+		panic(err)
+	}
+	return g
+}
+
 func (g *SliceNMeasure) VertexNo() uint {
 	return uint(g.slc.Len())
 }
@@ -38,4 +64,52 @@ func (g *SliceNMeasure) Weight(fromIdx, toIdx uint) interface{} {
 	f, t := g.slc.Index(int(fromIdx)), g.slc.Index(int(toIdx))
 	d := g.m.Call([]reflect.Value{f, t})
 	return d[0].Interface()
+}
+
+type RSubgraph struct {
+	g   RGraph
+	vls []uint
+}
+
+var _ RGraph = (*RSubgraph)(nil)
+
+func (g *RSubgraph) VertexNo() uint {
+	return uint(len(g.vls))
+}
+
+func (g *RSubgraph) Directed() bool {
+	return g.g.Directed()
+}
+
+func (g *RSubgraph) Weight(fromIdx, toIdx uint) interface{} {
+	fromIdx = g.vls[fromIdx]
+	toIdx = g.vls[toIdx]
+	return g.Weight(fromIdx, toIdx)
+}
+
+type WSubgraph struct {
+	g   WGraph
+	vls []uint
+}
+
+var _ WGraph = (*WSubgraph)(nil)
+
+func (g *WSubgraph) VertexNo() uint {
+	return uint(len(g.vls))
+}
+
+func (g *WSubgraph) Directed() bool {
+	return g.g.Directed()
+}
+
+func (g *WSubgraph) Weight(fromIdx, toIdx uint) interface{} {
+	fromIdx = g.vls[fromIdx]
+	toIdx = g.vls[toIdx]
+	return g.Weight(fromIdx, toIdx)
+}
+
+func (g *WSubgraph) SetWeight(fromIdx, toIdx uint, w interface{}) {
+	fromIdx = g.vls[fromIdx]
+	toIdx = g.vls[toIdx]
+	g.SetWeight(fromIdx, toIdx, w)
 }
---
+++ tsp_2opt_test.go
@@ -0,0 +1,136 @@
+package groph
+
+import (
+	"fmt"
+	"testing"
+)
+
+func pathEq(p1, p2 []uint) (bool, string) {
+	plen := uint(len(p1))
+	if plen != uint(len(p2)) {
+		return false, fmt.Sprintf("length differs: %d / %d",
+			plen,
+			uint(len(p2)))
+	}
+	s2 := uint(0)
+	for s2 < plen {
+		if p1[0] == p2[s2] {
+			break
+		}
+		s2++
+	}
+	if s2 >= plen {
+		return false, fmt.Sprintf("no start %d in p2=%v", p1[0], p2)
+	}
+	if p1[1] == p2[s2+1%plen] {
+		if s2 += 2; s2 >= plen {
+			s2 = 0
+		}
+		for s1 := uint(2); s1 < plen; s1++ {
+			if p1[s1] != p2[s2] {
+				return false, fmt.Sprintf("difference in pos %d / %d: %v %v",
+					s1, s2,
+					p1, p2)
+			}
+			if s2 += 1; s2 >= plen {
+				s2 = 0
+			}
+		}
+	} else {
+		if s2 == 0 {
+			s2 = plen - 1
+		} else {
+			s2--
+		}
+		for s1 := uint(1); s1 < plen; s1++ {
+			if p1[s1] != p2[s2] {
+				return false, fmt.Sprintf("difference in pos %d / %d: %v %v",
+					s1, s2,
+					p1, p2)
+			}
+			if s2 == 0 {
+				s2 = plen - 1
+			} else {
+				s2--
+			}
+		}
+	}
+	return true, ""
+}
+
+func Test2OptDAgaintsGreedy(t *testing.T) {
+	var points []point
+	var am *AdjMxDf32
+	for sz := uint(4); sz < 12; sz++ {
+		points = randomPoints(sz, points)
+		am = CpWeights(
+			NewAdjMxDf32(sz, am),
+			NewSliceNMeasure(points, dist, false).Verify(),
+		).(*AdjMxDf32)
+		gPath, gWeight := am.TspGreedy()
+		tPath, tWeight := Tsp2Optf32(am)
+		if tWeight/gWeight > 1.01 {
+			t.Errorf("size %d: different path length: greedy=%f / 2-opt=%f",
+				sz,
+				gWeight,
+				tWeight)
+		}
+		if ok, msg := pathEq(gPath, tPath); !ok {
+			t.Logf("size %d: %s", sz, msg)
+		}
+	}
+}
+
+func Test2OptUAgaintsGreedy(t *testing.T) {
+	var points []point
+	var am *AdjMxUf32
+	var dm *AdjMxDf32
+	for sz := uint(4); sz < 12; sz++ {
+		points = randomPoints(sz, points)
+		am = CpWeights(
+			NewAdjMxUf32(sz, am),
+			NewSliceNMeasure(points, dist, false).Verify(),
+		).(*AdjMxUf32)
+		dm = CpWeights(
+			NewAdjMxDf32(sz, dm),
+			NewSliceNMeasure(points, dist, false).Verify(),
+		).(*AdjMxDf32)
+		gPath, gWeight := dm.TspGreedy()
+		tPath, tWeight := Tsp2Optf32(am)
+		if tWeight/gWeight > 1.05 {
+			t.Errorf("size %d: different path length: greedy=%f / 2-opt=%f",
+				sz,
+				gWeight,
+				tWeight)
+		}
+		if ok, msg := pathEq(gPath, tPath); !ok {
+			t.Logf("size %d: %s", sz, msg)
+		}
+	}
+}
+
+func BenchmarkTsp2OptGenf32D(b *testing.B) {
+	sz := uint(120)
+	points := randomPoints(sz, nil)
+	am := CpWeights(
+		NewAdjMxDf32(sz, nil),
+		NewSliceNMeasure(points, dist, false).Verify(),
+	).(*AdjMxDf32)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Tsp2Optf32(am)
+	}
+}
+
+func BenchmarkTsp2OptGenf32U(b *testing.B) {
+	sz := uint(120)
+	points := randomPoints(sz, nil)
+	am := CpWeights(
+		NewAdjMxUf32(sz, nil),
+		NewSliceNMeasure(points, dist, false).Verify(),
+	).(*AdjMxUf32)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Tsp2Optf32(am)
+	}
+}
